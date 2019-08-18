ce488328003ee8f64125fc735885f744ff9a5370
mk-nazemi <nazemi.works@gmail.com>
2019-08-18 14:46:56 +0430

    add git lab

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <button @click="getSelectedRows()">Get Selected Rows</button>
     <ag-grid-vue
-      style="width: 500px; height: 500px;"
+      style="width: auto; height: 500px;"
       class="ag-theme-balham"
       :columnDefs="columnDefs"
       :rowData="rowData"
@@ -94,22 +94,26 @@ export default {
     AgGridVue
   },
   beforeMount() {
-    // this.columnDefs = [
-    //   { headerName: "Make", field: "make", sortable: true, filter: true },
-    //   { headerName: "Model", field: "model", sortable: true, filter: true },
-    //   { headerName: "Price", field: "price", sortable: true, filter: true }
-    // ];
+    this.columnDefs = [
+      {
+        headerName: Sh_ManagerConfirm_List,
+        field: "make",
+        sortable: true,
+        filter: true
+      },
+      { headerName: "Model", field: "model", sortable: true, filter: true },
+      { headerName: "Price", field: "price", sortable: true, filter: true }
+    ];
     // this.columnDefs = [
     //   { headerName: "Make", field: "make", checkboxSelection: true },
     //   { headerName: "Model", field: "model" },
     //   { headerName: "Price", field: "price" }
     // ];
-    this.columnDefs = Sh_ManagerConfirm_List;
-    // this.rowData = [
-    //   { make: "Toyota", model: "Celica", price: 35000 },
-    //   { make: "Ford", model: "Mondeo", price: 32000 },
-    //   { make: "Porsche", model: "Boxter", price: 72000 }
-    // ];
+    this.rowData = [
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxter", price: 72000 }
+    ];
     // fetch("https://api.myjson.com/bins/15psn9")
     //   .then(result => result.json())
     //   .then(rowData => (this.rowData = rowData));

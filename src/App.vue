261f4076f15567c02ecac27ddac844b653484864
Hessein Nazemi <nazemi.works@gmail.com>
2019-08-18 14:30:17 +0430

    add object loadable to component

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,68 @@ import { AgGridVue } from "ag-grid-vue";
 
 export default {
   name: "App",
+  props: [
+    {
+      Sh_ManagerConfirm_List: [
+        {
+          name: "EumManagerConfirmLicence",
+          required: true,
+          label: "نوع مجوز تأیید مدیران",
+          align: "right",
+          field: "EumManagerConfirmLicence",
+          sortable: true
+        },
+        {
+          name: "UserName",
+          required: true,
+          label: "کاربر",
+          align: "right",
+          field: "UserName",
+          sortable: true
+        },
+        {
+          name: "ConfirmationDate",
+          required: true,
+          label: "تاریخ",
+          align: "right",
+          field: "ConfirmationDate",
+          sortable: true
+        },
+        {
+          name: "ConfirmationTime",
+          required: true,
+          label: "ساعت",
+          align: "right",
+          field: "ConfirmationTime",
+          sortable: true
+        },
+        {
+          name: "CI_ResourceManagerConfirm",
+          required: true,
+          label: "منبع تأیید ",
+          align: "right",
+          field: "CI_ResourceManagerConfirm",
+          sortable: true
+        },
+        {
+          name: "CI_ResourceManagerConfirmDetails",
+          required: true,
+          label: "جزئیات منبع تأیید  ",
+          align: "right",
+          field: "CI_ResourceManagerConfirmDetails",
+          sortable: true
+        },
+        {
+          name: "Comments",
+          required: true,
+          label: "توضیحات",
+          align: "right",
+          field: "Comments",
+          sortable: true
+        }
+      ]
+    }
+  ],
   data() {
     return {
       columnDefs: null,
@@ -37,19 +99,20 @@ export default {
     //   { headerName: "Model", field: "model", sortable: true, filter: true },
     //   { headerName: "Price", field: "price", sortable: true, filter: true }
     // ];
-    this.columnDefs = [
-      { headerName: "Make", field: "make", checkboxSelection: true },
-      { headerName: "Model", field: "model" },
-      { headerName: "Price", field: "price" }
-    ];
+    // this.columnDefs = [
+    //   { headerName: "Make", field: "make", checkboxSelection: true },
+    //   { headerName: "Model", field: "model" },
+    //   { headerName: "Price", field: "price" }
+    // ];
+    this.columnDefs = Sh_ManagerConfirm_List;
     // this.rowData = [
     //   { make: "Toyota", model: "Celica", price: 35000 },
     //   { make: "Ford", model: "Mondeo", price: 32000 },
     //   { make: "Porsche", model: "Boxter", price: 72000 }
     // ];
-    fetch("https://api.myjson.com/bins/15psn9")
-      .then(result => result.json())
-      .then(rowData => (this.rowData = rowData));
+    // fetch("https://api.myjson.com/bins/15psn9")
+    //   .then(result => result.json())
+    //   .then(rowData => (this.rowData = rowData));
   },
   methods: {
     onGridReady(params) {
@@ -72,4 +135,8 @@ export default {
 <style lang="scss">
 @import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
 @import "../node_modules/ag-grid-community/dist/styles/ag-theme-balham.css";
+@import "../node_modules/ag-grid-community/src/styles/ag-grid.scss";
+@import "../node_modules/ag-grid-community/src/styles/ag-theme-balham/sass/ag-theme-balham.scss";
+// @import "../node_modules/ag-grid-community/src/styles/ag-grid.scss";
+// @import "../node_modules/ag-grid-community/src/styles/ag-theme-balham-dark/sass/ag-theme-balham-dark.scss";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import 'ag-grid-enterprise';
+// import 'ag-grid-enterprise';
 import App from "./App.vue";
 
 Vue.config.productionTip = false;

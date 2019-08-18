1f91bae451c7ca095963b4bc0bed9bf21e3ecc9e
Hessein Nazemi <nazemi.works@gmail.com>
2019-08-18 12:09:16 +0430

    make sample grid

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <button @click="getSelectedRows()">Get Selected Rows</button>
+    <ag-grid-vue
+      style="width: 500px; height: 500px;"
+      class="ag-theme-balham"
+      :columnDefs="columnDefs"
+      :rowData="rowData"
+      rowSelection="multiple"
+      @grid-ready="onGridReady"
+    ></ag-grid-vue>
   </div>
 </template>
 
+
+//$ npm install --save ag-grid-community ag-grid-vue vue-property-decorator
+// import SafaGrid from "./components/SafaGrid.vue";
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { AgGridVue } from "ag-grid-vue";
 
 export default {
-  name: "app",
+  name: "App",
+  data() {
+    return {
+      columnDefs: null,
+      rowData: null,
+      gridApi: null,
+      columnApi: null
+    };
+  },
   components: {
-    HelloWorld
+    AgGridVue
+  },
+  beforeMount() {
+    // this.columnDefs = [
+    //   { headerName: "Make", field: "make", sortable: true, filter: true },
+    //   { headerName: "Model", field: "model", sortable: true, filter: true },
+    //   { headerName: "Price", field: "price", sortable: true, filter: true }
+    // ];
+    this.columnDefs = [
+      { headerName: "Make", field: "make", checkboxSelection: true },
+      { headerName: "Model", field: "model" },
+      { headerName: "Price", field: "price" }
+    ];
+    // this.rowData = [
+    //   { make: "Toyota", model: "Celica", price: 35000 },
+    //   { make: "Ford", model: "Mondeo", price: 32000 },
+    //   { make: "Porsche", model: "Boxter", price: 72000 }
+    // ];
+    fetch("https://api.myjson.com/bins/15psn9")
+      .then(result => result.json())
+      .then(rowData => (this.rowData = rowData));
+  },
+  methods: {
+    onGridReady(params) {
+      this.gridApi = params.api;
+      this.columnApi = params.columnApi;
+    },
+    getSelectedRows() {
+      const selectedNodes = this.gridApi.getSelectedNodes();
+      const selectedData = selectedNodes.map(node => node.data);
+      const selectedDataStringPresentation = selectedData
+        .map(node => node.make + " " + node.model)
+        .join(", ");
+      alert(`Selected nodes: ${selectedDataStringPresentation}`);
+    }
   }
 };
 </script>
 
+
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
+@import "../node_modules/ag-grid-community/dist/styles/ag-theme-balham.css";
 </style>
